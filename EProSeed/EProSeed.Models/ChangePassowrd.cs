e4fa780dc21dd349d07b21492e2c98aa93dcef43
Induction <induction@prowareness.com>
2016-12-23 20:02:35 +0530

    kji

---
+++ EProSeed/EProSeed.Models/ChangePassowrd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EProSeed.Models
+{
+    public class ChangePassowrd
+    {
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string CurrentPassowrd { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+        [Required]
+        public string ConfirmPassowrd { get; set; }
+    }
+}
