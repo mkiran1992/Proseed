6949d4e7ed8f5f29362bfa55586c12e42260548a
sundar <sundarvadivel.s@gmail.com>
2016-12-28 19:06:02 +0530

    Trainer was not correct

---
+++ EProSeed/EProSeed.Lib/BLL/Contracts/ITrainer.cs
@@ -0,0 +1,28 @@
+﻿using EProSeed.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EProSeed.Lib.BLL
+{
+    public enum UserType : int
+    {
+        None = 0,
+        Trainer = 1,
+        Trainee = 2
+    }
+    public interface ITrainer
+    {
+        TrainerModel Login(string Email, string Password);
+        TrainerModel Login(string Email, string Password, out UserType userType);
+
+        string GetName(int id);
+
+        IList<TrainerModel> GetAll();
+
+        TrainerModel Find(int? id);
+    }
+}
---
+++ EProSeed/EProSeed.Lib/BLL/Repository/Trainer.cs
@@ -0,0 +1,104 @@
+﻿using EProSeed.DAL;
+using EProSeed.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EProSeed.Lib.BLL
+{
+    public class Trainer : ITrainer
+    {
+        protected readonly ProDbContext db;
+        public Trainer()
+        {
+            db = new ProDbContext();
+        }
+        /// <summary>
+        /// Avoid to use it, instead use another version.
+        /// </summary>
+        /// <param name="Email"></param>
+        /// <param name="Password"></param>
+        /// <returns></returns>
+        public TrainerModel Login(string Email, string Password)
+        {
+            try
+            {
+                var User = db.Tranner.Where(t => t.Email == Email && t.Password == Password).SingleOrDefault();
+
+                if (User != null)
+                {
+
+                    return User;
+
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public TrainerModel Login(string Email, string Password, out UserType UserType)
+        {
+            UserType = UserType.None;
+            try
+            {
+                var User = db.Tranner.Where(t => t.Email == Email && t.Password == Password).SingleOrDefault();
+
+                if (User != null)
+                {
+
+                    UserType = (UserType)db.TrainerTraineeUserMapping.Where(M => User.Id == M.Map_Trainer_Id).Select(M => M.Map_UserType_Id).SingleOrDefault();
+
+                    return User;
+
+                }
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public int Create(TrainerModel trainer)
+        {
+            db.Tranner.Add(trainer);
+            db.SaveChanges();
+            return trainer.Id;
+        }
+
+        public string GetName(int id)
+        {
+            try
+            {
+                var tranner = db.Tranner.Where(t => t.Id == id).SingleOrDefault();
+                return tranner.Name;
+            }
+            catch (Exception Ex)
+            {
+                throw new Exception(Ex.Message);
+            }
+
+        }
+
+        public TrainerModel Find(int? id)
+        {
+            var tranner = db.Tranner.Where(t => t.Id == id).SingleOrDefault();
+            return tranner;
+        }
+
+        public IList<TrainerModel> GetAll()
+        {
+            int user_type = UserType.Trainer.GetHashCode();
+            return db.Tranner.Where(n => db.TrainerTraineeUserMapping.Any(k =>
+                k.Map_Trainer_Id == n.Id
+                && k.Map_UserType_Id == user_type)).ToList();
+        }
+
+    }
+}
